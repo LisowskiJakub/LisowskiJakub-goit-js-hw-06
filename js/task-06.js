0cf50c66db77c1d2b31410c2feb2a4b905d1f4bd
Jakub Lisowski <lisowski.jakub@gmail.com>
2023-02-18 14:05:43 +0100

    resolved 6 homework

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const validationInput = document.querySelector('#validation-input');
+validationInput.addEventListener('blur', (e) => {
+
+    if (Number(validationInput.dataset.length) === e.target.value.length) {
+        validationInput.classList.add('valid');
+        validationInput.classList.remove('invalid');
+    }
+    else {
+        validationInput.classList.add('invalid');
+        validationInput.classList.remove('valid');
+    }
+})
